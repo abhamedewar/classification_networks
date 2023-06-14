a2f5e5c843ec5afd75810258aadb83c325682245
Abhishri <abhamedewar@gmail.com>
2023-06-13 20:56:12 -0700

    added implementation of all the vgg architectures

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+'''
+This repository contains implementations of Convolutional Neural Network (CNN)
+popular architectures from scratch.
+
+Currently the repository supports the following CNN architectures:
+1. LeNet
+2. VGGNet
+    VGG-11
+    VGG-13
+    VGG-16
+    VGG-19
+
+The codebase also provides support for conducting exploratory data analysis on the custom dataset.
+The training module is integrated with tensorboard to visualize training loss, validation loss,
+training accuracy, validation accuracy, intermediate training images, weights of layer in the model.
+
+'''
 import argparse
-import imp
+import torchvision
 from torchvision import transforms
 import torch
 from torch import nn
@@ -11,14 +28,15 @@ import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from load_data import CustomDataset
 from models.lenet import LeNet
+from models.vgg import VGG
 from eda import dataset_stats, plot_class_distribution, visualize_samples
-
+from torch.utils.tensorboard import SummaryWriter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', default=r'.\AID_combine', type=str)
 parser.add_argument('--csv_path', default=r'.\aid_dataset.csv', type=str)
 parser.add_argument('--class_mapping', default=r'.\class_mapping.json', type=str)
-parser.add_argument('--network_type', default='lenet', type=str)
+parser.add_argument('--network_type', default='lenet', type=str, choices=['lenet', 'vgg11', 'vgg13', 'vgg16', 'vgg19'])
 args = parser.parse_args()
 
 with open(args.class_mapping, "r") as json_file:
@@ -26,25 +44,17 @@ with open(args.class_mapping, "r") as json_file:
 
 data_df = pd.read_csv(args.csv_path)
 
-# custom_dataset = CustomDataset(args.csv_path, args.data_path)
-# #checking if CustomDataset is working or not
-
-# fig = plt.figure()
-# for i, sample in enumerate(custom_dataset):
-#     plt.imshow(sample[0])
-#     plt.show()
-#     if i == 3:
-#         break
-
-# exit()
+vgg = {'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'], 
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], 
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']}
 
 #EDA
+dataset_stats(data_df, class_map)
+plot_class_distribution(data_df, class_map)
+visualize_samples(data_df, args.data_path, class_map)
 
-# dataset_stats(data_df, class_map)
-# plot_class_distribution(data_df, class_map)
-# visualize_samples(data_df, args.data_path, class_map)
-
-
+#setup device to be used
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 num_classes = len(class_map)
 
@@ -53,22 +63,31 @@ batch_size = 64
 learning_rate = 0.001
 num_epochs = 1
 
-if args.network_type == 'lenet':
-    model = LeNet(num_classes=num_classes)
-    model.to(device)
-    resize = transforms.Resize(32)
 
 #if image coloured
 # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
 #if image is Grayscale
 normalize = transforms.Normalize(mean=[0.5], std=[0.5])
-transform = transforms.Compose([
+
+if args.network_type == 'lenet':
+    model = LeNet(num_classes=num_classes)
+    model.to(device)
+    transform = transforms.Compose([
                 transforms.Grayscale(),
-				resize,
+				transforms.Resize(32),
 				transforms.ToTensor(),
 				normalize,
 			])
+
+elif 'vgg' in args.network_type:
+    in_channels = 3
+    model = VGG(in_channels, num_classes, vgg[args.network_type])  
+    transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            normalize,
+        ])  
 
 custom_dataset = CustomDataset(args.csv_path, args.data_path, transform)
 split = [int(len(custom_dataset)*0.9), int(len(custom_dataset)*0.1)]
@@ -76,13 +95,16 @@ train_set, valid_set = torch.utils.data.random_split(custom_dataset, split)
 trainloader = DataLoader(train_set, batch_size, shuffle=True)
 validloader = DataLoader(train_set, batch_size, shuffle=True)
 
-
 #loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+#tensorboard summary writer
+writer = SummaryWriter(f'runs/classification')
+
 def train(model, trainloader, num_epoch):
     print("Start training...")
+    i = 0
     for i in tqdm(range(num_epoch)):
         model.train()
         for batch, label in tqdm(trainloader):
@@ -91,20 +113,36 @@ def train(model, trainloader, num_epoch):
             optimizer.zero_grad()
             pred = model(batch)
             loss = criterion(pred, label)
+            writer.add_scalar("Loss/train", loss, i)
             loss.backward()
             optimizer.step()
+            #visualize images 
+            img_grid = torchvision.utils.make_grid(batch)
+            writer.add_image('train/images', img_grid, i)
+            #visualize weights of the convolution layer 3
+            writer.add_histogram('conv 3', model.conv3.weight.detach().cpu().numpy(), i)
+            curr_correct = (torch.argmax(pred, dim=1) == label).sum().item()
+            curr_train_acc = float(curr_correct)/float(batch.shape[0])
+            writer.add_scalar("Accuracy/train", curr_train_acc, i)
+            i += 1
 
 def evaluate(model, validloader):
     model.eval() 
     correct = 0
+    i = 0
     with torch.no_grad():
         for batch, label in tqdm(validloader):
             batch = batch.to(device)
             label = label.to(device)
             pred = model(batch)
-            correct += (torch.argmax(pred, dim=1) == label).sum().item()
-    acc = correct/len(validloader.dataset)
-    print("Evaluation accuracy: {}".format(acc))
+            loss = criterion(pred, label)
+            writer.add_scalar("Loss/validation", loss, i)
+            correct = (torch.argmax(pred, dim=1) == label).sum().item()
+            valid_acc = correct/len(validloader.dataset)
+            writer.add_scalar("Accuracy/validation", valid_acc, i)
+            i += 1
+    # acc = correct/len(validloader.dataset)
+    # print("Evaluation accuracy: {}".format(acc))
 
 train(model, trainloader, num_epochs)
 evaluate(model, validloader)
---
+++ models/vgg.py
@@ -0,0 +1,53 @@
+from torch import nn
+import torch
+
+class VGG(nn.Module):
+    '''
+    This class implements the VGGNet architecture for image classification.
+    VGGNet is known for its deep architecture with 11, 13, 16 or 19 weight 
+    layers and the use of small 3x3 convolutional filters.
+    Args:
+        num_classes: The number of output classes.
+    '''
+    def __init__(self, in_channels, num_classes, vgg_layers):
+
+        super(VGG, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
+        self.vgg_layer_info = vgg_layers
+        self.conv_layers = self.architecture()
+        self.fc1 = nn.Linear(in_features=512*7*7, out_features=4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.fc3 = nn.Linear(4096, num_classes)
+        self.dropout = nn.Dropout(p = 0.5)
+        self.relu = nn.ReLU()
+    
+    def forward(self,x):
+        
+        x = self.conv_layers(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.dropout(self.relu(self.fc1(x)))
+        x = self.dropout(self.relu(self.fc2(x)))
+        x = self.fc3(x)
+
+        return x
+
+    def architecture(self):
+        layers = []
+        in_channels = self.in_channels
+        for x in self.vgg_layer_info:
+            if type(x) == int:
+                out_channels = x
+                layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1))
+                layers.append(nn.ReLU())
+                in_channels = x
+            if type(x) == str:
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+
+        return nn.Sequential(*layers)
+
+# test vgg
+# x = torch.randn(64, 3, 224, 224)
+# model = VGG(3, 30, [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'])
+# print(model)
+# print(model(x).shape) ----------> should give 64*30
